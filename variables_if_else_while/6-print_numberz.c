e8cd12bfec7cd9d3fa94c31bc58011fd7b4f77a2
Neia2 <7276@holbertonstudents.com>
2023-10-18 02:03:21 -0700

    print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print numberz from 0
+* Return: this returns 0
+*/
+
+int main(void)
+{
+	int i;
+		for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
