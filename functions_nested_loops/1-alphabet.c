ba2bb68d0ba57f5a9334f4fb306d0907a59bd93e
Neia2 <7276@holbertonstudents.com>
2023-10-18 07:07:51 -0700

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,14 +5,13 @@
 *Return: this returns 0
 */
 
-int main(void)
+void print_alphabet(void)
 {
 	int i;
-		for (i = 'a' ; i <= 'z' ; i++)
 
-		{
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
 		_putchar('\n');
-		}
-
-	return (0);
 }
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*main - prints 10 times the alphabet, in lowercase
+*Return: Always 0
+*/
+
+int main(void)
+{
+
+
