8bc47758b5c7318efdddf1b946bf9db9449f52ce
Neia2 <7276@holbertonstudents.com>
2023-10-22 05:00:14 -0700

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * mul - Multiplies two integers
+ * @a: First operand
+ * @b: Second operand
  *
- * Return: Always 0.
+ * Return: Product of both operands
  */
-int main(void)
+int mul(int a, int b)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	int mul;
+
+	mul = a * b;
+	return (mul);
 }
