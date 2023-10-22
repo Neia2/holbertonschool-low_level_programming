eb0d149421bb5de4e42c1331808d3510c8d3dd3f
Neia2 <7276@holbertonstudents.com>
2023-10-22 01:08:56 -0700

    is upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isupper - prog checks for uppercase character
+* @c: Variable text
+* Return: 1 for upper letter or 0 for any else
+*/
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'z')
+		{
+		return (1);
+		}
+	else
+		{
+		return (0);
+		}
+}
