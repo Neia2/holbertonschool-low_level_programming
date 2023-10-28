8e233981cbebfb1cacc66c2dd1298eb8634198eb
Neia2 <7276@holbertonstudents.com>
2023-10-28 09:00:52 -0700

    string toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changing string to upper function
+ * @n: string pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+			i++;
+		}
+	}
+	return (n);
+}
