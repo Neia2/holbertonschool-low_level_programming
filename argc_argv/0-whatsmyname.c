e47e8863b72120162c5a5f87e212ce4bc86f0a94
Neia2 <7276@holbertonstudents.com>
2023-11-03 07:13:23 -0700

    its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
-* 
-* @arc, argv: arguments to be executed.
+ * main -  prints its name.
+* @argv: command to print its name of a string.
+* @argc: command not used.
 * Return: its name
 */
 
 int main(__attribute__((unused))int argc, char *argv[])
-{	
-	printf ("%s\n", *argv);
+{
+	printf("%s\n", *argv);
 
 	return (0);
 }
