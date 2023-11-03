35dc57c83b158fd2f05d7cc0da1c1e303821c8f4
Neia2 <7276@holbertonstudents.com>
2023-11-03 07:02:28 -0700

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* 
+* @arc, argv: arguments to be executed.
+* Return: its name
+*/
+
+int main(__attribute__((unused))int argc, char *argv[])
+{	
+	printf ("%s\n", *argv);
+
+	return (0);
+}
