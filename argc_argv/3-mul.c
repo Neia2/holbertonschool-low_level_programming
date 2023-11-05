169dc66dc3505a323021aa599aec179fcb2d2603
Neia2 <7276@holbertonstudents.com>
2023-11-05 13:39:33 -0800

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-        printf("Error\n");
+	printf("Error\n");
 	return (1);
 	}
 	if (argc == 3)
-		result = atoi(argv[1]) * atoi(argv[2]);
+	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 
 	return (0);
--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,34 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main: adds positive numbers passed as command line arguments.
+ * main - adds positive numbers.
  *
- * @argc: The number of command line arguments.
- * @argv: An array of strings representing the command line arguments.
- * @return: 0 if successful, 1 if there is an error.
- */
-int main(int argc, char* argv[])
-{
-	int sum = 0;
-	int i, j;
-
-
-	if (argc == 1)	
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{	
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{ 
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-		
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
-}
