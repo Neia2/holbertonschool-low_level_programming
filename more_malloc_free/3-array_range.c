25631dced5f3191388c1509787fb7c524e1f17e4
Neia2 <7276@holbertonstudents.com>
2023-11-09 07:21:10 -0800

    array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,12 +13,16 @@ int *array_range(int min, int max)
 	int *array, i, size;
 
 	if (min > max)
-	return (NULL);
+		return (NULL);
+
 	size = max - min + 1;
 	array = malloc(sizeof(int) * size);
+
 	if (array == NULL)
-	return (NULL);
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = min++;
+
 	return (array);
 }
