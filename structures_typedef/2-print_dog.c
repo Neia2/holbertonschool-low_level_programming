44bca0b69f5669d935cc756951782c79ca6bfc54
Neia2 <7276@holbertonstudents.com>
2023-11-13 06:37:45 -0800

    init dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * Write a function that prints a struct dog
+ * @d: prints a struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	printf("nil");
+
+	else if ( name == NULL)
+		printf("Name: (nil)");
+	
+	else (d == NULL);
+		printf(" ");
+}
