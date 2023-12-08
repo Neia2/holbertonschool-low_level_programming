b8cdd04b85692148dc2cf848c3117fdc9cf0405b
Neia <nenaslife@gmail.com>
2023-12-08 14:48:08 +0000

    append_text_to_file

--- file_io/main.h
+++ file_io/main.h
@@ -4,10 +4,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+
+#endif /* MAIN_H */
