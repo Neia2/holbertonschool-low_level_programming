8ba84239c3f460b3115adf768fbd9de47b9f0291
Neia2 <nenaslife@gmail.com>
2024-02-23 15:31:14 +0100

    test

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*print_alphabet: Write a function that prints alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
+
---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ *
+ * Return:  result.
+ */
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
