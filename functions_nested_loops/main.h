3e086f91dd5ec7a6f2e8b0ff97a64f963b3fdd31
Neia2 <nenaslife@gmail.com>
2024-02-23 15:54:04 +0100

    push mai nh

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void print_to_98(int n);
+int add(int a, int b);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int n);
+int _abs(int c);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char c);
