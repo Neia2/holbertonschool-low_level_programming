3602083abda44f4440b4688f54a01f17a65b5260
Neia2 <nenaslife@gmail.com>
2024-03-03 18:51:55 +0100

    strncat

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - converts string to uppercase from lower
+ * @s: char pointer
+ * string_toupper: converts string to uppercase from lower
+ *
+ * Return: char pointer
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i]  >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
+
