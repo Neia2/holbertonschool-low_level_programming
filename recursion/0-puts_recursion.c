a743811a405559f57282eda940f8f2430b0d359b
Neia2 <nenaslife@gmail.com>
2024-03-06 09:36:33 +0100

    print rev recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string recursively.
+ * @s: The string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
