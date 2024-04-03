aa8d57c4bbcb113282019cf29405ae5513ccdfb7
Neia2 <nenaslife@gmail.com>
2024-04-03 06:50:49 +0200

    update betty rules

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include "lists.h"
 
-struct node
-{
-	int data;
-	struct node *link;
-};
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: A pointer to the head of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
 
 size_t list_len(const list_t *h)
 {
