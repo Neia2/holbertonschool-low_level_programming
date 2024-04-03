b64f967ec182415f325302be4600647cf5e53037
Neia2 <nenaslife@gmail.com>
2024-04-03 10:52:25 +0200

    add node update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,24 +13,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	{
+
 	struct list_s *new_node = malloc(sizeof(struct list_s));
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	new_node->str = strdup(str);
+new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
 	return (*head);
-	}
 }
