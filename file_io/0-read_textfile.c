9bf811e18df3c979c9a5be9e6cc08d555c7db6b7
Neia2 <nenaslife@gmail.com>
2024-05-03 14:04:57 +0200

    updating betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,35 +10,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, bytes_read, bytes_written;
+	char *buf;
+	ssize_t fd, bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-   fd = open(filename, O_RDONLY);
-    if (fd == -1)
-      return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-	 if (buf == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-   bytes_read = read(fd, buf, letters);
+	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1)
 	{
 		close(fd);
 		free(buf);
 		return (0);
 	}
-   bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-	close(fd);
-   free(buf); 
-   return (0);
+		close(fd);
+		free(buf);
+		return (0);
 	}
 close(fd);
 free(buf);
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,36 +10,36 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int writen;
-    int len_append;
+	int fd;
+	int writen;
+	int len_append;
 
-    len_append = 0;
+	len_append = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content == NULL)
-    {
-        close(fd);
-        return (1);
-    }
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
-    while (text_content[len_append] != '\0')
-        len_append++;
+	while (text_content[len_append] != '\0')
+		len_append++;
 
-    writen = write(fd, text_content, len_append);
-    if (writen == -1)
-    {
-        close(fd);
-        return (-1);
-    }
+	writen = write(fd, text_content, len_append);
+	if (writen == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
