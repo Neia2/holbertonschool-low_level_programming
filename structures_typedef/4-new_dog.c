1eb28610cbf85c7cee305eed1711135fdd5a8bef
Neia2 <nenaslife@gmail.com>
2024-05-03 15:43:02 +0200

    updating files

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,46 +3,51 @@
 #include "dog.h"
 
 /**
- * dog_t - creates a new dog.
- * @owner:owner's name
+ * new_dog - creates a new dog.
  * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
  * Return: Pointer to the new dog structure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, j;
+	dog_t *new_dog;
 
-	if(name == NULL || owner == NULL)
-		return(NULL);
-
-	for (i = 0; name[i] != '\0'; i++)
-
-	for (j = 0; owner[j] !='\0'; j++)
-
-	dog_*new_dog = malloc(sizeof(dog_t));
-
-	if (new_dog) == (NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	new_dog->name = malloc(i+1);
-	new_dog->owner = malloc(j+1);
+	for (i = 0; name[i] != '\0'; i++)
+	 {}
 
-	if (new_dog->name == NULL || new_dog->owner == NULL) {
-	free(new_dog->name);
-	free(new_dog->owner);
-	free(new_dog);
-	return (NULL);
+	for (j = 0; owner[j] != '\0'; j++)
+	 {}
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(i + 1);
+	new_dog->owner = malloc(j + 1);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
 	}
 
 	for (i = 0; name[i] != '\0'; i++)
-		new_dog->name[i] = name[i];
+	new_dog->name[i] = name[i];
 	new_dog->name[i] = '\0';
 
 	for (j = 0; owner[j] != '\0'; j++)
-		new_dog->owner[j] = owner[j];
+	new_dog->owner[j] = owner[j];
 	new_dog->owner[j] = '\0';
 
 	new_dog->age = age;
 
-    	return (new_dog);
+	return (new_dog);
 }
