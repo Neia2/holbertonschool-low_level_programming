63dcf2fdeaa3af667e4771dbed19010c6884b129
Neia2 <nenaslife@gmail.com>
2024-05-03 13:45:16 +0200

    files io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    char *buf;
+    ssize_t fd, bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+   fd = open(filename, O_RDONLY);
+    if (fd == -1)
+      return (0);
+
+    buf = malloc(sizeof(char) * letters);
+	 if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+   bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+   bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+	close(fd);
+   free(buf); 
+   return (0);
+	}
+close(fd);
+free(buf);
+return (bytes_written);
+}
